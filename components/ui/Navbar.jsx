6d26aa9b79a24ab5d9d6b320c92a7cae6fb9aac6
mansabhashim <mansabkhan491@gmail.com>
2024-07-27 04:54:18 -0700

    deployed

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -1,19 +1,19 @@
-'use client'
-import React from 'react'
-import Image from 'next/image'
+// 'use client'
+// import React from 'react'
+// import Image from 'next/image'
 
-const Navbar = () => {
-  return (
-    <div>
-        <img src=' ../public ' alt="Lo0go" />
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Contact</li>
-        </ul>
-    </div>
-  )
-}
+// const Navbar = () => {
+//   return (
+//     <div>
+//         <img src=' ../public ' alt="Lo0go" />
+//         <ul>
+//             <li>Home</li>
+//             <li>About</li>
+//             <li>Services</li>
+//             <li>Contact</li>
+//         </ul>
+//     </div>
+//   )
+// }
 
-export default Navbar
+// export default Navbar
