4cfd48a8fa7c6867c016a4a16b22b5a7906bc39a
mansabhashim <mansabkhan491@gmail.com>
2024-08-07 13:54:37 -0700

    responsive

--- components/ui/Btn.jsx
+++ components/ui/Btn.jsx
@@ -7,13 +7,13 @@ const Btn = (props) => {
     <a
     href="tel:888-451-6551"
       id="btn"
-      className="bg-secondary hover:bg-primary hover:text-darker border-secondary border-2 flex items-center gap-0 justify-evenly btn md:w-[250px] w-[220px] h-16 rounded-lg px-2 py-3"
+      className="bg-secondary hover:bg-primary hover:text-darker border-secondary border-2 flex items-center gap-0 justify-evenly btn md:w-[250px] w-[160px] md:h-16 h-12 rounded-lg px-2 py-3"
     >
-      <FaPhoneSquareAlt className='text-5xl bg-re-500' />{" "}
-      <div className="btn text-black">
+      <FaPhoneSquareAlt className='md:text-5xl text-3xl bg-re-500' />{" "}
+      <div className="btn text-black ">
        
-       <p> 888-451-6551       </p>
-       <p> Click To Call Us </p>
+       <p > 888-451-6551       </p>
+       <p > Click To Call Us </p>
       </div>
       
     </a>
--- components/ui/LearnMore.jsx
+++ components/ui/LearnMore.jsx
@@ -7,13 +7,13 @@ const LearnMore = () => {
     <Link
     href="/contact"
     id="learn" 
-      className="bg-darker flex items-center justify-evenly gap-0  text-secondary hover:bg-secondary hover:text-darker hover:border-primary border-secondary border-2 rounded-lg px-2 w-[150px] h-16 py-3"
+      className="bg-darker flex items-center justify-evenly hover:text-black gap-0  text-secondary hover:bg-secondary hover:text-darker hover:border-primary border-secondary border-2 rounded-lg px-2 md:w-[160px] w-[120px] md:h-16 h-12 py-3"
     >
       {" "}
-      <div className="learnmore">
-        Learn More
+      <div className="learnmore md:text-xl text-sm  flex items-center justify-between gap-1 text-secondary">
+       <p className='text-secondary hover:text-black'> Learn More</p>
+      <FaArrowRight className='lrnmre'/>
       </div>
-      <FaArrowRight/>
     </Link>
   );
 };
